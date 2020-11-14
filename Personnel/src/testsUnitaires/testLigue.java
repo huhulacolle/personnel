2d8f206e1c8f337811007a8eeaa0a925338b8746
Hugo <huhu051@gmail.com>
2020-11-14 16:42:20 +0100

    Ajout d'un test unitaire pour la modification des informations d'un employe

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -1,6 +1,9 @@
 package testsUnitaires;
 
+import static commandLineMenus.rendering.examples.util.InOut.getString;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -23,6 +26,21 @@ class testLigue
 	{
 		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "01/01/2020", "02/02/2021", "azerty"); 
+		assertEquals(employe, ligue.getEmployes().first());
+	}
+	
+	@Test
+	void modifEmploye() throws SauvegardeImpossible 
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "01/01/2020", "02/02/2021", "azerty"); 
+		System.out.println(employe);
+		employe.setNom("Bouchard2");
+		employe.setPrenom("Gérard2");
+		employe.setMail("rule@34.com");
+		employe.setDatedebut("01/01/2001");
+		employe.setDatefin("02/02/2002");
+		employe.setPassword("qwerty");
 		assertEquals(employe, ligue.getEmployes().first());
 	}
 }
