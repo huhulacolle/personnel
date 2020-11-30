a25582df03eba6ecfa1e47cde59e19977c434c4c
rthiriot <rthiriot@D41-04.MDNPEDAGO.local>
2020-11-30 09:38:44 +0100

    Test unitaire provisoire pour root devenant admin iteration 4 oui je suis en avance mais osef

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -76,5 +76,21 @@ class testLigue
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com",LocalDate.parse("2010-09-09"), LocalDate.parse("2020-10-01"), "azerty");
 		ligue.setAdministrateur(employe);
 		assertEquals(employe,ligue.getAdministrateur());
+		
 	}
+	@Test
+	void changeAdmintoroot() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Flechettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com",LocalDate.parse("2010-09-09"), LocalDate.parse("2020-10-01"), "azerty");
+		ligue.getAdministrateur();
+		if (ligue.getAdministrateur() == null) {
+		ligue.setAdministrateur(gestionPersonnel.getRoot());
+		assertEquals(gestionPersonnel.getRoot(),ligue.getAdministrateur());
+		}
+		else {
+			ligue.setAdministrateur(employe);
+			assertEquals(employe,ligue.getAdministrateur());
+		}
+		}
 }
